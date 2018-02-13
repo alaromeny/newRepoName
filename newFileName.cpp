20f39f9f8dfbcff9bb228a5ded19f6c22d782276
Siobhan Duncan <siobhan.duncan.trumpet@gmail.com>
2018-02-13 11:05:02 +0000

    I created a new file and with a comment
    I created a new file and initialised it with a comment, as part of an example for the slides for class

---
+++ newFileName.cpp
@@ -0,0 +1,11 @@
+/**********************************
+ *
+ *
+ *   My New Repo: 
+ *   Edited: 12/02/18
+ *   Author: Siobhan Duncan
+ *   Notes: Just an example for the class
+ *
+ *
+ **********************************/
+  
